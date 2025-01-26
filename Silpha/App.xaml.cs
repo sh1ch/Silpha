e3d5047c9bf37526587a12bc7375046983c4a04d
sh1ch <isogucchi-code@outlook.com>
2025-01-26 22:46:35 +0900

    feat: プロジェクトを追加

---
+++ Silpha/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Silpha;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Syldra.WinUI.SyldraApp
+{
+	protected override void ConfigureViewServices(IServiceCollection services)
+	{
+		services.AddTransient<MainWindow>();
+	}
+
+	protected override void ConfigureUserServices(IServiceCollection services)
+	{
+		// ユーザーデータを追加
+	}
+
+	protected override Window CreateWindow() => ViewContainer.GetRequiredService<MainWindow>();
+}
